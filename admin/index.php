e8428308aa3ea05b8155507b65faf5c1f6dc9e0a
langonginc <langonginc@yeah.net>
2020-05-27 18:30:22 +0800

    Update auto

--- admin/index.php
+++ admin/index.php
@@ -14,34 +14,36 @@ include "includes.php";
   <body>
     <h1>Config your blog</h1>
     <br>
-    <form action="update.php" method="post">
-      <table>
-        <tr>
-          <td>Blog name 博客名称</td>
-          <td><input name="name" type="text" value="<?php echo $config['name'] ?>"></td>
-        </tr>
-        <tr>
-          <td>Blog describe 博客描述</td>
-          <td><input name="des" type="text" value="<?php echo $config['land'] ?>"></td>
-        </tr>
-        <tr>
-          <td>Posts 文章数量</td>
-          <td><?php echo $config['post'] ?></td>
-        </tr>
-        <?php
-          for ($i = 1; $i <= $config['post']; $i ++)
-          {
-            $ntit = $posttit[$i];
-            echo "
-            <tr>
-              <td>Post NO.$i 文章 $i</td>
-              <td><a href=\"edit.php?pid=$i\">$ntit</a></td>
-            </tr>
-            ";
-          }
-        ?>
-      </table>
-      <input type="submit" value=" Go ">
-    </form>
+    <div class="admin">
+      <form action="update.php" method="post">
+        <table>
+          <tr>
+            <td>Blog name 博客名称</td>
+            <td><input name="name" type="text" value="<?php echo $config['name'] ?>"></td>
+          </tr>
+          <tr>
+            <td>Blog describe 博客描述</td>
+            <td><input name="des" type="text" value="<?php echo $config['land'] ?>"></td>
+          </tr>
+          <tr>
+            <td>Posts 文章数量</td>
+            <td><?php echo $config['post'] ?></td>
+          </tr>
+          <?php
+            for ($i = 1; $i <= $config['post']; $i ++)
+            {
+              $ntit = $posttit[$i];
+              echo "
+              <tr>
+                <td>Post NO.$i 文章 $i</td>
+                <td><a href=\"edit.php?pid=$i\">$ntit</a></td>
+              </tr>
+              ";
+            }
+          ?>
+        </table>
+        <input type="submit" value=" Go ">
+      </form>
+    </div>
   </body>
 </html>
